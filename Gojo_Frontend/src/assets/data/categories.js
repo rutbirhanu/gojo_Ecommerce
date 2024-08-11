6d64715c6b88973385b3c730344c54cb7044c2ef
Rut Birhanu <birhanurut@gmail.com>
2024-08-11 16:48:23 +0300

    categories data

---
+++ Gojo_Frontend/src/assets/data/categories.js
@@ -0,0 +1,45 @@
+export const NavgationCategories = [
+    {
+        "title": "Men",
+        "image":""
+    },
+    {
+        "title": "Women",
+        "image":""
+    },
+    {
+        "title": "Toys",
+        "image":""
+    },
+    {
+        "title": "Electronics",
+        "image":""
+    },
+    {
+        "title": "Cosmetics",
+        "image":""
+    },
+    {
+        "title": "Sport",
+        "image":""
+    },
+    {
+        "title":  "Home",
+        "image":""
+    },
+
+]
+
+export const Categories = [
+    
+]
+
+export const Items=[
+    {
+        "name":"sneaker shoe",
+        "category":"Sneaker",
+        "description":"this is the description",
+        "price":"900",
+        "image":""
+    }
+]
