3529cbe79f049c3dc1369b15d4ce8143d9e8a8f0
Rut Birhanu <birhanurut@gmail.com>
2024-08-17 13:23:34 +0300

    details page description

--- Gojo_Frontend/src/App.jsx
+++ Gojo_Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import HomePage from "./pages/homePage"
+// import HomePage from "./pages/homePage"
+import DetailsPage from "./pages/DetailsPage";
 function App() {
 
   return (
     <Routes>
-      <Route exact path="/" element={<HomePage/>} />
+      {/* <Route exact path="/" element={<HomePage/>} /> */}
+      <Route exact path="/" element={<DetailsPage/>} />
     </Routes>
   )
 }
---
+++ Gojo_Frontend/src/pages/DetailsPage.jsx
@@ -0,0 +1,48 @@
+import './pages.css'
+import TitleComponent from '../components/TitleComponent'
+import ItemCard from "../components/ItemCard"
+
+
+function DetailsPage() {
+    return (
+      <div className="details-parent-container">
+             <div className='details-page-container'>
+          <div className="details-left-side">
+              <img src="/gallery-1.jpg" alt="apple watch "/>
+          </div>
+
+          <div className="details-right-side">
+              <h3>8 Topshop Brown Plant Print Top</h3>
+              <h3>$12.00</h3>
+                    <div className="item-details">
+                        <h4>Details</h4>
+                       <div className="detail">
+                        <span>Condition</span>
+                        <span>Good</span>
+                    </div>
+                    <div className="detail">
+                        <span>Brand</span>
+                        <span>TopShop</span>
+                    </div>
+                    <div className="detail">
+                        <span>Category</span>
+                        <span>Women</span>
+                    </div>
+                    <div className="detail">
+                        <span>Tops & Blouses</span>
+                        <span>Blouse</span>
+                    </div>
+                     </div>
+          </div>
+            </div>
+            <br/>
+            <br/>
+            <div className="similar-items">
+              <TitleComponent title="More From This Category" />
+              <ItemCard name="apple watch" image="/gallery-1.jpg" price="200" />
+          </div>
+      </div>
+  )
+}
+
+export default DetailsPage
